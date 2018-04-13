e50de56d50a9288db217b916d5b3d1ef875ee7bf
cooperalex97 <37555743+cooperalex97@users.noreply.github.com>
2018-04-13 09:57:50 -0500

    Update prob3.cpp

--- prob3.cpp
+++ prob3.cpp
@@ -1,87 +1,56 @@
 #include <iostream>
+
 using namespace std;
 
-///EJERCICIO 03--- Merge Sort
-void MergeSort(int inicio,int fin,int *lista){
-    if(fin - inicio == 0 || fin - inicio == 1)/// Se declara una excepcion por si la lista es vacia o de un solo elemento
-        return;                             /// Retorna la misma
-    int medio= (inicio + fin)/2;/// Se define el punto medio de la lista
-    MergeSort(inicio,medio,lista);/// Se divide en sublista a la izquierda
-    MergeSort(medio,fin,lista);/// Se divide en sublista a la derecha
-
-    int puntero1 = inicio,
-        puntero2 = medio,
-        puntero3 = 0;
-
-    int array[fin-inicio];/// Se define la mitad de la lista tomada
-
-    ///Mezclamos las sub-lista de derecha y de izquierda, en el arreglo array.
-    while(puntero1<medio || puntero2<fin){
-        if(puntero1<medio && puntero2<fin){
-            if(lista[puntero1]<lista[puntero2]){
-                array[puntero3++] = lista[puntero1++];
-            }else{
-                array[puntero3++] = lista[puntero2++];
-            }
-        }else if(puntero1<medio){
-            array[puntero3++] = lista[puntero1++];
-        }else{
-            array[puntero3++] = lista[puntero2++];
-        }
-    }
-    for(int i=0;i<fin-inicio;i++){
-        lista[inicio+i]=array[i];
-    }
-}
-///EJERCICIO 03 --- Quick Sort
-
-
-void leeCadena(int cant,int n[])
+void ingresarE(int A[100],int &n)
 {
-    int i;
-    for(i=0;i<cant;i++)
+  cout<<"Numero de elementos del array: ";cin>>n;
+
+  for(int i=0;i<n;i++)
+  {
+    cout<<"Elemento ["<<i<<"]: ";
+    cin>>A[i];
+  }
+
+  cout<<"Tu array creado es : \n";
+  cout<<"A =[";
+  for(int i=0;i<n;i++){
+    cout<<A[i]<<" ";
+  }
+  cout<<"]";
+}
+
+void insertion_sort(int A[],int n)
+{
+  int i, key;
+  for (int j=1;j<n;j++)
+  {
+    key=A[j];
+    i=j-1;
+    while(i>=0 && A[i]>key)
     {
-        cout<<"Ingresa numero "<<i+1<<": ";
-        cin>>n[i];
+      A[i+1]=A[i];
+      i=i-1;
     }
+    A[i+1]=key;
+  }
+  
 
 }
 
-void muestraCadena(int cant,int n[])
+void print_(int A[], int n)
 {
-    int i;
-    for(i=0;i<cant;i++)
-    {
-        cout<<n[i]<<endl;
-    }
+  for(int k = 0;A[k] != '\0';k++)
+  {
+    cout<<A[k];
+  }
 }
-void quicksort(int A[],int izq, int der )
+
+int main()
 {
-int i, j, x , aux;
-i = izq;
-j = der;
-x = A[ (izq + der) /2 ];
-    do{
-        while( (A[i] < x) && (j <= der) )
-        {
-            i++;
-        }
-
-        while( (x < A[j]) && (j > izq) )
-        {
-            j--;
-        }
-
-        if( i <= j )
-        {
-            aux = A[i]; A[i] = A[j]; A[j] = aux;
-            i++;  j--;
-        }
-
-    }while( i <= j );
-
-    if( izq < j )
-        quicksort( A, izq, j );
-    if( i < der )
-        quicksort( A, i, der );
+  int A[100],n;
+  ingresarE(A,n);
+  insertion_sort(A,n);
+  print_(A,n);
+  return 0;
 }
